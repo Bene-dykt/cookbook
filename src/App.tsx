0bb3350312139a3ba788ff546e49a35ac89678f2
Bene-dykt <eckerstorfer.benedikt@gmail.com>
2026-07-01 13:28:01 +0200

    setup deployment

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import {auth} from "./firebase.ts";
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <section id="center">
